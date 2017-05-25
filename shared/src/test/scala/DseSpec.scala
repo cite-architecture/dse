6ca20dbf60b92f2fdfd3098aa99c3b2c426777ff
Neel Smith <dnsmith.neel@gmail.com>
2017-05-25 13:25:28 -0400

    Define pending tests

--- shared/src/test/scala/DseSpec.scala
+++ shared/src/test/scala/DseSpec.scala
@@ -9,7 +9,17 @@ class DseSpec extends FlatSpec {
 
 
 
-  "A Digital Scholarly Edition" should "do things" in pending
+  "A Digital Scholarly Edition" should "be instantiated from a CiteLibrary" in pending
+
+  it should "identify the set of TBS in the library" in pending
+  it should "identify the set of texts in the library" in pending
+  it should "identify the set of image collections in the library" in pending
+  it should "find images for a given TBS" in pending
+  it should "find images for a given text passage" in pending
+  it should "find text passages for a given TBS" in pending
+  it should "find text passages for a given image" in pending
+  it should "find TBS illustrated by a given image" in pending
+  it should "be possible to instantiate from a CEX source" in pending
 
 
 }
