d6395b080a98beef73c5b067d7caa4c82b96a7ba
Neel Smith <dnsmith.neel@gmail.com>
2017-05-25 14:10:02 -0400

    Add DseSource class

---
+++ jvm/src/main/scala/DseSource.scala
@@ -0,0 +1,28 @@
+package edu.holycross.shot.dse
+
+import edu.holycross.shot.scm._
+import scala.io.Source
+
+
+import scala.scalajs.js
+import js.annotation.JSExport
+
+
+
+/** Factory for making catalogs from text sources.
+*/
+object DseSource {
+
+
+  /** Create [[Dse]] from a file with data in CEX format.
+  *
+  * @param fileName Name of file with data in CEX format.
+  */
+  def fromFile(fileName: String): Dse = {
+    val lns = Source.fromFile(fileName).getLines.toVector
+    Dse(lns.mkString("\n"))
+  }
+
+
+
+}
